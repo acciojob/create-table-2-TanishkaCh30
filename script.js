100a0bd6f81634168b5e5af9d2c3af2522606bc9
TanishkaCh30 <137798637+TanishkaCh30@users.noreply.github.com>
2025-02-20 22:44:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
 function createTable() {
-    //Write your code here
+   let table = document.querySelector("#myTable");
+	let rows = prompt("Input number of rows");
+	let columns = prompt("Input number of columns");
+
+	if(isNaN(rows) || isNaN(columns)){
+		return;
+}
+	if(rows<=0 || columns<=0){
+		alert("Enter values greater than 0");
+		return;
+	}
+	for(let i = 0 ; i<rows ; i++){
+		let row = document.createElement("tr");
+		for(let j = 0 ; j<columns ; j++){
+			let column = document.createElement("td");
+			column.innerText = `Row-${i} Column-${j}`;
+			row.append(column);
+}
+		table.append(row);
+	}
   
 }
+
+  
+
+
+  
